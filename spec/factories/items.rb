3837e2c8633ea7552e0ca096d2c42004417df081
syousyuukoriki <5364tarou@gmail.com>
2020-11-09 15:53:52 +0900

    itemモデルのテストコードを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name           {"カメラ"}
+    introduction        {"カメラです"}
+    category_id         {2}
+    item_condition_id   {2}
+    price               {300}
+    shipping_cost_id    {2}
+    area_id             {1}
+    days_id             {2}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
