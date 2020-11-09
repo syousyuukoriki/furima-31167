a93f26921c50951634b62370b893d00470efd398
syousyuukoriki <5364tarou@gmail.com>
2020-11-09 13:55:06 +0900

    商品出品機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+  
+
+
+  private 
+  def item_params
+    params.require(:item).permit( :item_name, :introduction, :category_id, :item_condition_id, :price, :shipping_cost_id, :area_id, :days_id, :image ).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :day
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+
+  with_options presence: true do
+    validates :item_name
+    validates :introduction
+    validates :category_id,        numericality: { other_than: 1 } 
+    validates :item_condition_id,  numericality: { other_than: 1 } 
+    validates :price,              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :shipping_cost_id,   numericality: { other_than: 1 } 
+    validates :area_id,            numericality: { other_than: 0 } 
+    validates :days_id,            numericality: { other_than: 1 } 
+    validates :user
+    validates :image
+  end
+
 end
