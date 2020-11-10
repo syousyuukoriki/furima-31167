74ed6d3714926ba17e4fe1e89634aeaa956078c5
syousyuukoriki <5364tarou@gmail.com>
2020-11-10 16:02:05 +0900

    itemsテーブルのdays_idカラムの名前をday_idに変更

--- db/migrate/20201108031725_create_items.rb
+++ db/migrate/20201108031725_create_items.rb
@@ -8,7 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer     :price,             null: false
       t.integer     :shipping_cost_id,  null: false
       t.integer     :area_id,           null: false
-      t.integer     :days_id,           null: false
+      t.integer     :day_id,            null: false
       t.references  :user,              foreign_key: true
       t.timestamps
     end
