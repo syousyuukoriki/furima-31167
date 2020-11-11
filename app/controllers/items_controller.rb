1f2dcd914389f5509ba3289b4ae248961848c36e
syousyuukoriki <5364tarou@gmail.com>
2020-11-11 12:31:27 +0900

    編集ページへ遷移できる人を制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only:[:edit]
+  
   def index
     @items = Item.order('created_at DESC')
   end
@@ -22,7 +23,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+
   private
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
 
   def item_params
     params.require(:item).permit(:item_name, :introduction, :category_id, :item_condition_id, :price, :shipping_cost_id, :area_id, :day_id, :image).merge(user_id: current_user.id)
