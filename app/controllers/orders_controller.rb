1b72bd0bf66e9efcc9975007341bc3ece6b59c6d
syousyuukoriki <5364tarou@gmail.com>
2020-11-13 12:26:32 +0900

    購入情報と配達先情報が一つのフォームでそれぞれのテーブルに保存されるようにした

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+
+  def index
+   @order_destination = OrderDestination.new
+  end
+
+  def create
+    @order_destination = OrderDestination.new(destination_params)
+    if @order_destination.valid?
+        @order_destination.save
+        redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+    def destination_params
+      params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    end 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_11_083534) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "order_id"
     t.datetime "created_at", precision: 6, null: false
