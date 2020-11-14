a0a01386ffc42a6177b77f1933e232f9727bdd94
syousyuukoriki <5364tarou@gmail.com>
2020-11-14 17:34:32 +0900

    クレジットカード決済機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,14 @@ class OrdersController < ApplicationController
 
   def index
    @order_destination = OrderDestination.new
+   @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_destination = OrderDestination.new(destination_params)
     if @order_destination.valid?
+        pay_item
         @order_destination.save
         redirect_to root_path
     else
@@ -16,7 +19,15 @@ class OrdersController < ApplicationController
 
   private
     def destination_params
-      params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+      params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
     end 
 
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create( 
+        amount: @item.price,
+        card: destination_params[:token],
+        currency: 'jpy'
+      )
+    end
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,6 +1,6 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format:{with:/\A\d{3}[-]\d{4}\z/}
@@ -8,6 +8,7 @@ class OrderDestination
     validates :city
     validates :address
     validates :phone_number, format:{with:/\A[0-9]+\z/}
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
