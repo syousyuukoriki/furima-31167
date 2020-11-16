f6db0571e0cca7fd652b96863216bb2da29fea33
syousyuukoriki <5364tarou@gmail.com>
2020-11-16 12:41:13 +0900

    電話番号を11文字以内に制限

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -7,7 +7,7 @@ class OrderDestination
     validates :prefecture_id
     validates :city
     validates :address
-    validates :phone_number, format:{with:/\A[0-9]+\z/}
+    validates :phone_number, format:{with:/\A[0-9]+\z/}, length: {maximum: 11}
     validates :token
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
