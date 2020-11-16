3e7d16ed6d9b7f9fefd04fffcc634418f4292d6e
syousyuukoriki <5364tarou@gmail.com>
2020-11-16 16:43:35 +0900

    before_actionの実行される順番を入れ替えることによりmove_indexないの共通の処理の記述を削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
   
   before_action :authenticate_user!, only: [:index]
-  before_action :move_to_index, only: [:index]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
+  
 
   def index
    @order_destination = OrderDestination.new
@@ -25,7 +26,6 @@ class OrdersController < ApplicationController
     end 
 
     def move_to_index
-      @item = Item.find(params[:item_id])
       redirect_to root_path  unless @item.order.nil?
 
       if current_user.id == @item.user.id
