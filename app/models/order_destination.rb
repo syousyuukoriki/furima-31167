71c5afe2917b87fe24532bc554d8823f23a68a9b
syousyuukoriki <5364tarou@gmail.com>
2020-11-16 16:43:57 +0900

    不要な記述を削除

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,6 +1,6 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format:{with:/\A\d{3}[-]\d{4}\z/}
